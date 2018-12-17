125155a3812acb147ef2ea2ae7e503f9fb6377d4
samar <samar.shirazi@yahoo.com>
2018-12-17 07:26:03 +0500

    added operator precedence examples

---
+++ lecture-1/operator_precedance.c
@@ -0,0 +1,14 @@
+# include <stdio.h>
+
+int main(){
+    printf("%d\n", 24 - 4 + 2);
+    printf("%d\n", 24 - 4 * 2);
+    printf("%d\n", 3 * 6 / 2);
+    printf("%d\n", 24 / 4 / 2);
+
+    printf("%d\n", 24 - 11 % 4);
+
+    printf("%d\n", 24 / 11 % 4);
+
+    return 0;
+}
--- lecture-1/operators.c
+++ lecture-1/operators.c
@@ -5,7 +5,7 @@ int main()
    int sum = a + b ;
    printf("sum of a=%d and b=%d is %d \n", a, b, sum);
    int sub = a - b ;
-   printf("subtraction of a=%d and b=%d is %d \n ", a, b, sub);
+   printf("subtraction of a=%d and b=%d is %d \n", a, b, sub);
 
    int mul = a * b ;
    printf("product of a=%d and b=%d is %d \n", a, b, mul);
@@ -16,15 +16,17 @@ int main()
    float div_float = a / b ;
    printf("division of a=%d and b=%d is %f \n", a, b, div_float);
 
-    float aa = 31;
+   float aa = 31;
    float div_float_int = aa / b ;
    printf("division of aa=%f and b=%d is %f \n", aa, b, div_float_int);
 
-    float bb = 15;
+   float bb = 15;
    float div_int_float = a / bb ;
    printf("division of a=%d and bb=%f is %f \n", a, bb, div_int_float);
 
-    int remainder = -7 % 10;
-    printf("remainder of a=%d and b=%d is %d \n", a, b, remainder);
+   int remainder = -7 % 10;
+   printf("remainder of a=%d and b=%d is %d \n", a, b, remainder);
+
+
    return 0;
 }
